39af8efea79783ecc31c8e37518787bfa52f6246
DevTDavicho <developer2023Davicho@outlook.es>
2024-07-22 08:07:40 -0500

    Added Auto model import and implemented CRUD operations (create, update, read, delete) for autos in AutoService

--- 2B_Aplicación_Web/Semana_13-AWeb/Classwork_S13/cw1_S13/concesionario/src/app/services/auto.service.ts
+++ 2B_Aplicación_Web/Semana_13-AWeb/Classwork_S13/cw1_S13/concesionario/src/app/services/auto.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { Global } from "./global";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { Auto } from '../models/autos';
 
 @Injectable()
 export class AutoService {
@@ -17,5 +18,35 @@ export class AutoService {
   getAutos ():Observable<any> {
     let headers = new HttpHeaders().set('Content-Type', 'application/json');
     return this._http.get(this.url + 'autos', {headers: headers});
+  }
+
+  //! Friday
+  // Guardar auto
+  // http://localhost:3700/guardar-auto
+  guardarAuto(auto: Auto): Observable<any>{
+    let params = JSON.stringify(auto);
+        let headers = new HttpHeaders().set('Content-Type','application/json');
+        return this._http.post(this.url + 'guardar-auto', params, {headers:headers});
+  }
+
+  //editar auto
+  //http://localhost:3600/auto/:id
+  updateAuto(auto: Auto): Observable<any>{
+    let params = JSON.stringify(auto);
+    let headers = new HttpHeaders().set('Content-Type','application/json');
+    return this._http.put(this.url + 'auto/' + auto._id, params, {headers:headers});
+  }
+
+  //ver un auto
+  //http://localhost:3600/auto/:id
+  getAuto(id: String):Observable<any>{
+    let headers = new HttpHeaders().set('Content-Type','application/json');
+    return this._http.get(this.url + 'auto/' + id, {headers:headers});
+  }
+  //eliminar un auto
+  //http://localhost:3600/auto/:id
+  deleteAuto(id: String):Observable<any>{
+      let headers = new HttpHeaders().set('Content-Type','application/json');
+      return this._http.delete(this.url+'auto/' + id, {headers:headers});
   }
 }
