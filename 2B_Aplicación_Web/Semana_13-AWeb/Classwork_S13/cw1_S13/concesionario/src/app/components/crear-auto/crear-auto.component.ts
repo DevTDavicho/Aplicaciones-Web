bc9067bec4506bb61a5d46c99b5ad09dda8a0795
DevTDavicho <developer2023Davicho@outlook.es>
2024-07-22 08:21:36 -0500

    Added imports, properties, and methods to CrearAutoComponent, including a constructor, a form submission handler, and an image change event handler.

--- 2B_Aplicación_Web/Semana_13-AWeb/Classwork_S13/cw1_S13/concesionario/src/app/components/crear-auto/crear-auto.component.ts
+++ 2B_Aplicación_Web/Semana_13-AWeb/Classwork_S13/cw1_S13/concesionario/src/app/components/crear-auto/crear-auto.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { AutoService } from 'src/app/services/auto.service';
+import { CargarService } from 'src/app/services/cargar.service';
+import { Auto } from '../../models/autos';
+import { Global } from '../../services/global';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-crear-auto',
   templateUrl: './crear-auto.component.html',
-  styleUrls: ['./crear-auto.component.css']
+  styleUrls: ['./crear-auto.component.css'],
+  providers: [AutoService, CargarService]
 })
 export class CrearAutoComponent {
+  public titulo: string;
+  public auto: Auto;
+  public autoGuardar: Auto;
+  public url: string;
+  public status: string;
+  public idGuardado: string;
+  public archivosParaCargar: Array<File>;
+  @ViewChild('archivoImagen') fileInput: any
+  constructor(
+    private _autoService: AutoService,
+    private _cargarService: CargarService
+  ){
+    this.titulo="/GUARDAR AUTO";
+    this.url = Global.url;
+    this.auto = new Auto('','','','',20,10,'');
+    this.autoGuardar = new Auto('','','','',20,10,'');
+    this.status = "";
+    this.idGuardado = "";
+    this.archivosParaCargar = [];
+  }
 
+  guardarAuto(form: NgForm){
+    this._autoService.guardarAuto(this.auto).subscribe(
+      response => {
+        if(response.auto){
+          if(this.archivosParaCargar){
+            this._cargarService.peticionRequest(Global.url + "subir-imagen/" + response.auto._id, [], this.archivosParaCargar, 'imagen')
+            .then((result: any)=>{
+              this.autoGuardar = result.response;
+              this.status = "success";
+              console.log(response.auto._id);
+              this.idGuardado=response.auto._id;
+              form.reset();
+              this.fileInput.nativeElement.value = '';
+            });
+          }else{
+            this.status = 'failed';
+          }
+        }else{
+          this.status = 'failed';
+        }
+      },
+      error => {
+        console.log(<any>error);
+      }
+    )
+  }
+  imagenChangeEvent(archivoSeleccionado: any){
+    this.archivosParaCargar = <Array<File>>archivoSeleccionado.target.files;
+  }
 }
