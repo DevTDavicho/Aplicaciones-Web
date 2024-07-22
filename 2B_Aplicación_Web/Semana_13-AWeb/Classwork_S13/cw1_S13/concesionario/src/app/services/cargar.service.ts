977a9c2fc359f5fa8d9e4dcbbc429961d119a237
DevTDavicho <developer2023Davicho@outlook.es>
2024-07-22 08:13:46 -0500

    Refactor CargarService: removed unused imports and methods, added constructor, and implemented peticionRequest method using XMLHttpRequest

--- 2B_Aplicación_Web/Semana_13-AWeb/Classwork_S13/cw1_S13/concesionario/src/app/services/cargar.service.ts
+++ 2B_Aplicación_Web/Semana_13-AWeb/Classwork_S13/cw1_S13/concesionario/src/app/services/cargar.service.ts
@@ -1,20 +1,32 @@
-// import { Injectable } from "@angular/core";
-// import { Global } from "./global";
-// import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { Global } from './global';
+@Injectable()
+export class CargarService{
+  public url: string;
+  constructor(){
+    this.url = Global.url;
+  }
 
-// @Injectable()
-// export class AutoService {
-//   public url: string;
-//   constructor (
-//     private _http: HttpClient
-//   ){
-//     this.url = Global.url;
-//   }
+  peticionRequest(url: string, params: Array<string>, files: Array<File>, name: string){
+    return new Promise(function(resolve, reject){
+      var formData: any = new FormData();
+      var xhr = new XMLHttpRequest();
+      for(var i=0; i<files.length ;i++){
+        formData.append(name, files[i], files[i].name);
+      }
 
-//   // Ver autos
-//   // http://localhost:3700/autos
-//   getAutos () {
-//     let headers = new HttpHeaders().set('Content-Type', 'application/json');
-//     return this._http.get(this.url + 'autos', {headers: headers});
-//   }
-// }
+      xhr.onreadystatechange=function(){
+        if(xhr.readyState == 4){
+          if(xhr.status == 200){
+            resolve(JSON.parse(xhr.response));
+          }else{
+            reject(xhr.response);
+          }
+        }
+      }
+
+      xhr.open('POST', url, true);
+      xhr.send(formData);
+    })
+  }
+}
